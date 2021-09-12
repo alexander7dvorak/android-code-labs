6172b9ab016df8833d29473a59581c1e9d40ab53
alexander7dvorak <alexander7dvorak@gmail.com>
2021-09-13 01:45:04 +0300

    Added forgotten unsaved changes

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,11 +1,127 @@
-fun main(args: Array<String>) {
+import java.util.*
+
+/*fun main(args: Array<String>) {
     println("Hello, ${args[0]}")
 
     // Will assign kotlin.Unit
     val isUnit = println("This is an expression")
     println(isUnit)
-
+/*
     val temperature = 10
     val isHot = if (temperature > 50) true else false
     println(isHot)
+*/
+    val temperature = 10
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
 }
+*/
+
+/*
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println ("Today is $day and the fish eat $food")
+}
+*/
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun main() {
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    // eager, creates a new list
+    val eager = decorations.filter { it [0] == 'p' }
+    println("eager: $eager")
+    // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+/*
+    var dirtyLevel = 20
+    val waterFilter = { dirty : Int -> dirty / 2}
+    println(waterFilter(dirtyLevel))
+*/
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter))
+    println(updateDirty(15, ::increaseDirty))
+
+    var dirtyLevel = 19;
+    dirtyLevel = updateDirty(dirtyLevel) { dirtyLevel -> dirtyLevel + 23}
+    println(dirtyLevel)
+}
+
+fun increaseDirty( start: Int ) = start + 1
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+/*
+fun main(args: Array<String>) {
+    feedTheFish()
+    swim()   // uses default speed
+    swim("slow")   // positional argument
+    swim(speed="turtle-like")   // named parameter
+}
+*/
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else  -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
