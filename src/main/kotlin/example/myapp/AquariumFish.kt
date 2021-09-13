1535aad4f5bfab3643acc04cda1d829f9b181d57
alexander7dvorak <alexander7dvorak@gmail.com>
2021-09-13 14:23:10 +0300

    Added forgotten unsaved changes

--- src/main/kotlin/example/myapp/AquariumFish.kt
+++ src/main/kotlin/example/myapp/AquariumFish.kt
@@ -1,4 +1,51 @@
 package example.myapp
 
-class AquariumFish {
+interface FishAction  {
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+class Shark: FishAction, FishColor {
+    override val color = "gray"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+
+sealed class Seal
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal): String {
+    return when(seal) {
+        is Walrus -> "walrus"
+        is SeaLion -> "sea lion"
+    }
+}
+
+interface AquariumAction {
+    fun eat()
+    fun jump()
+    fun clean()
+    fun catchFish()
+    fun swim()  {
+        println("swim")
+    }
 }
